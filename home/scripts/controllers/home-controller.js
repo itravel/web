8a8186fb65ca16f4021d0f1bccf35cf688c5f4f1
weiminw <weiminw80105@gmail.com>
2014-09-29 18:45:38 +0800

    修改页面效果

--- home/scripts/controllers/home-controller.js
+++ home/scripts/controllers/home-controller.js
@@ -3,7 +3,7 @@
  */
 angular.module("home").controller("HomeController",['$scope','ActivityService',function($scope,ActivityService){
     $scope.activities = [];
-    ActivityService.getUneditDataPage(0,6).then(function(data){
+    ActivityService.getUneditDataPage(0,3).then(function(data){
         $scope.activities = data;
     })
     $scope.getImageThumbnail = function(activity){
